a5f9a7279004c632d52b0c0b4334e6ba67e9eec5
enzo ruiz <enzo.rp.90@gmail.com>
2017-04-06 22:27:13 -0500

    bodyparser y mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,6 +10,36 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended : false }))
 app.use(bodyParser.json())
 
-app.listen(port, () => {
-	console.log('API REST corriendo en http://localhost:'+port)
+app.get('/api/product/', (req, res) => {
+	res.status(200).send({products : []})
 })
+
+app.get('/api/product/:id', (req, res) => {
+
+})
+
+app.post('/api/product/', (req, res) => {
+	console.log(req.body)
+	res.status(200).send({message : 'producto actualizado'})
+})
+
+app.put('/api/product/:id', (req, res) => {
+
+})
+
+app.delete('/api/product/:id', (req, res) => {
+
+})
+
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+	if (err){
+		return console.log('Error en la conexion con la base de datos => ' + err)
+	}
+
+	console.log('Conexion con la base de datos establecida...')
+
+	app.listen(port, () => {
+		console.log('API REST corriendo en http://localhost:'+port)
+	})
+})
+
