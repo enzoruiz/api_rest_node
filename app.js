3ab0e719424e1c3ac91ced7878a0f3de3f599679
enzo ruiz <enzo.rp.90@gmail.com>
2017-04-27 18:53:58 -0500

    agregando handlebars para las vistas | creando vista de login y productos

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const api = require('./routes/index')
+const hbs = require('express-handlebars')
 
 app.use(bodyParser.urlencoded({ extended : false }))
 app.use(bodyParser.json())
 
+// USANDO HANDLEBARS PARA LAS VISTAS
+app.engine('.hbs', hbs({
+	default: 'default',
+	extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
 app.use('/api', api)
+
+app.use('/login', (req, res) => {
+	res.render('login')
+})
+
+app.use('/', (req, res) => {
+	res.render('products')
+})
 
 module.exports = app
